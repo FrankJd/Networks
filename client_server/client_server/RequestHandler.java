1ed9c74d2df03205df8fddea2cc414bd44b13351
Troy Nechanicky <nech5860@mylaurier.ca>
2018-01-26 17:22:44 -0500

    small updates to RFC, removed unneeded files, added server file, added gitignore

---
+++ client_server/client_server/RequestHandler.java
@@ -0,0 +1,9 @@
+import java.net.Socket;
+
+public class RequestHandler implements Runnable {
+
+	public RequestHandler(Socket connection) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ client_server/client_server/Server.java
@@ -0,0 +1,27 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 
+ * @throws Exception if portNumber arg is invalid
+ * 
+ * @author troyn
+ *
+ */
+public class Server {
+	public static void main(String argv[]) throws Exception {
+		int portNumber = new Integer(argv[0]);
+		//will throw exception if portNumber is invalid
+		ServerSocket socket = new ServerSocket(portNumber);
+
+		//continuously listen for connection requests
+		//create thread for each request
+		while (true) {
+			//listen and accept connection request
+			Socket connection = socket.accept();
+			RequestHandler request = new RequestHandler(connection);
+			Thread thread = new Thread(request);
+			thread.start();
+		}
+	}
+}
