06443f33348549ba46a1ad9166f091762019d94a
Frank K <31193895+FrankJd@users.noreply.github.com>
2018-02-15 11:33:13 -0500

    Update GUI.java

--- ReliableFileTransferProtocol/Receiver/GUI.java
+++ ReliableFileTransferProtocol/Receiver/GUI.java
@@ -1,1 +1,40 @@
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
+public class GUI {
+
+	Client client;
+	JFrame guiClient = new JFrame("Receiver");
+
+	public GUI(Receiver r) {
+
+
+		guiClient.setSize (660, 500);
+		guiClient.setResizable(false);
+    
+    
+  }
+  
+}
+
+	
